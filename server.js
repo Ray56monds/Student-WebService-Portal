9b289c788682c98bad03d10235ffd1c770a205b3
Ray56monds <magezir2@stanbic.com>
2024-03-13 22:48:37 +0300

    commit all changes in the server.js file

--- server.js
+++ server.js
@@ -8,16 +8,31 @@ const app = express();
 // Middleware
 app.use(morgan('dev'));
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'public')));
 
 // Routes
 app.use('/api/courses', courseRoutes);
 
-// Route for the root URL
+// Route handlers for HTML pages
 app.get('/', (req, res) => {
     res.send('Welcome to the Student Web Service Portal!');
 });
 
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'login.html'));
+});
+
+app.get('/node-course', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'node-course.html'));
+});
+
+app.get('/course', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'course.html'));
+});
+
+app.get('/about', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'about.html'));
+});
+
 // Start the server
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
