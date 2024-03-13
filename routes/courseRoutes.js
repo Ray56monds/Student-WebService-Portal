9cf6bba5984a6c056bcd9d4b8fc7f969e23b4173
Ray56monds <magezir2@stanbic.com>
2024-03-14 00:13:31 +0300

    commit changes in the courseRoutes.js

--- routes/courseRoutes.js
+++ routes/courseRoutes.js
@@ -1,6 +1,8 @@
 // Import required modules
 import express from 'express';
 import * as courseController from '../controllers/courseController.js';
+import { dirname, fileURLToPath } from 'url';
+import { join } from 'path';
 
 // Create router
 const router = express.Router();
@@ -14,3 +16,13 @@ router.delete('/courses/:courseId', courseController.deleteCourseById);
 
 // Export router
 export default router;
+
+// Function to get current directory name
+const getCurrentDirname = () => {
+    const filename = fileURLToPath(import.meta.url);
+    return dirname(filename);
+}
+
+// Set the views directory
+const viewsPath = join(getCurrentDirname(), '..', 'public');
+app.set('views', viewsPath);
