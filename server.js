f315a7c07513c816daebe12de9a7aef81a4ecb50
Ray56monds <magezir2@stanbic.com>
2024-03-13 17:59:18 +0300

    please commit the server.js file

--- server.js
+++ server.js
@@ -1,17 +1,18 @@
 // server.js
 
 import express from "express";
+import morgan from  "morgan";
 import courseRoutes from './routes/courseRoutes.js';
 
 const app = express();
 
 // Middleware
-app.use(morga('dev'));
+app.use(morgan('dev'));
 app.use(express.json());
 
 // Routes
-app.use('/api/courses', courseRoutes);
+app.use('/courses', courseRoutes);
 
-// Server
+// Start the server
 const PORT = 4000;
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
