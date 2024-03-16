7a87ca2a222ee132a02683a8fb62ebfa70290fd7
Ray56monds <magezir2@stanbic.com>
2024-03-16 15:34:23 +0300

    commit changes in server.js

--- server.js
+++ server.js
@@ -1,21 +1,28 @@
 import express from 'express';
+import path from 'path';
 import morgan from 'morgan';
-import path from 'path'
-import courseRoutes from './routes/courseRoutes.js';
 
-const __dirname = path.dirname(new URL(import.meta.url).pathname);
 const app = express();
+const __dirname = path.dirname(new URL(import.meta.url).pathname);
 
 // Middleware
 app.use(morgan('dev'));
 app.use(express.json());
 
-// Serve static files from the Public directory
-const publicPath = path.join(__dirname, 'public');
-app.use(express.static(publicPath));
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
 
-// Routes
-app.use('/api/courses', courseRoutes);
+// Route for the login page
+app.get('/login', (req, res) => {
+    const filePath = path.join(__dirname, 'public', 'pages', 'login.html');
+    res.sendfile(filePath);
+});
+
+// Route for the Node Course page
+app.get('/node-course', (req, res) => {
+    const filePath = path.join(__dirname, 'public', 'pages', 'node-course.html');
+    res.sendfile(filePath);
+});
 
 // Route for the root URL
 app.get('/', (req, res) => {
