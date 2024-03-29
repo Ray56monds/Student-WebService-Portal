4d7eba27a79ab1fb25cd10dd92dde65f0758c010
Ray56monds <magezir2@stanbic.com>
2024-03-29 11:35:36 +0300

    commit changes in the routes folder

--- routes/courseRoutes.js
+++ routes/courseRoutes.js
@@ -3,10 +3,7 @@ import * as courseController from '../controllers/courseController.js';
 
 const router = express.Router();
 
-<<<<<<< HEAD
-=======
 // Define routes for CRUD operations on courses
->>>>>>> bfc87c8474f2b88696ff493fe98f104874a918e1
 router.get('/', courseController.getAllCourses);
 router.get('/:courseId', courseController.getCourseById);
 router.post('/', courseController.createCourse);
